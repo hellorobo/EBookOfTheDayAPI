823168853b3ec30160dd72e07b8447a6d90b55dc
hellorobo <trebor.oknewokaj@gmail.com>
2018-06-19 23:27:52 +0200

    switched to db access

--- models/ebook.py
+++ models/ebook.py
@@ -1,35 +1,27 @@
 from datetime import datetime
-from bs4 import BeautifulSoup
-import requests
+from dbase import Database
 
+class EbookModelDB():
 
-class EbookModel():
+    def __init__(self):    
+        dbserver = os.environ['DB_SERVER']
+        dbname = os.environ['DB_NAME']
+        dbuser = os.environ['DB_USER']
+        dbpass = os.environ['DB_PASS']
 
-    a1 = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36'
-    a2 = ' (KHTML, like Gecko) Chrome/64.0.3282.186 Safari/537.36'
-    agent = {'User-Agent': a1 + a2}
+        con_str = pymongo.MongoClient('mongodb://{}:{}@{}/{}'.format(dbuser,dbpass,dbserver,dbname))
+        db = dbname
+        col_freebooks = 'freebooks'
+        col_logs = 'logs'
 
-    def __init__(self):
-        pass
+        freebooks = Database(con_str, db, col_freebooks)
+        logs = Database(con_str, db, col_logs)
+        today = datetime.utcnow()
 
-    def get(self, url):
-        self.url = url
-        resp = requests.get(self.url, headers=self.agent)
-        soup = BeautifulSoup(resp.text, 'html.parser')
-        self.title = soup.select('.dotd-title')[0].text.strip()
-        self.description = soup.find(
-            "div", class_="dotd-main-book-summary float-left").select(
-            "div")[2].text.strip()
-        dotd_image = soup.find("div", class_="dotd-main-book-image float-left")
-        self.image_src = "https:"+dotd_image.a.noscript.img.get('src')
-        dt = datetime.now()
-        self.date = dt.strftime("%Y-%m-%d %H:%M")
-
-        return self
-
-    def json(self):
-        return({"date": self.date,
-                "title": self.title,
-                "description": self.description,
-                "image_src": self.image_src
-                })
+    def get(self):
+            try:
+                return freebooks.find_record({"date": today.strftime("%Y-%m-%d")})
+                result = logs.insert_record({"date": today , "event": "record has been fetched from database"})
+            except:
+                return {"message": "Internal error occurred, while getting database record"}, 500
+                result = logs.insert_record({"date": today , "event": "error while getting database record"})
--- resources/ebook.py
+++ resources/ebook.py
@@ -5,7 +5,6 @@ from models.ebook import EbookModel
 class Ebook(Resource):
 
     def get(self):
-        url = 'https://www.packtpub.com/packt/offers/free-learning'
+        # url = 'https://www.packtpub.com/packt/offers/free-learning'
 
-        ebook = EbookModel().get(url)
-        return ebook.json()
+        return EbookModelDB().get()
